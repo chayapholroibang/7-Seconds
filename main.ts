62ca5cc5dbb128bab6dd66474bcbce25c50c720c
Chayaphol Roibang <89515745+chayapholroibang@users.noreply.github.com>
2021-10-09 14:47:31 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+let start = 0
+let elapsed = 0
+let score = 0
+input.onButtonPressed(Button.A, function () {
+    start = input.runningTime()
+    basic.showIcon(IconNames.Chessboard)
+})
+input.onButtonPressed(Button.B, function () {
+    elapsed = input.runningTime() - start
+    score = Math.abs(elapsed - 7000)
+})
+basic.forever(function () {
+	
+})
